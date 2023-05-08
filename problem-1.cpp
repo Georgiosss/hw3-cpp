b22f13a499b8e3badda7dccf593d7eb720e0b438
Georgiosss <59983624+Georgiosss@users.noreply.github.com>
2023-05-08 21:29:25 +0400

    Initial commit

---
+++ problem-1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int & x : arr) cin >> x;
+    /*  
+        სანამ ვინმე ასეთ რამეს დამიწერთ, მე დაგასწრებთ და თქვენ აღარ ქნათ :D
+
+        sort(arr.begin(), arr.end());
+        for (int x : arr)
+            cout << x << " ";
+        cout << endl;
+    */
+    return 0;
+}
